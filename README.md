62eee3acd98292a0b8ad816822836e79c2c51145
José Marques <jose.marques@talkdesk.com>
2020-03-24 17:42:59 +0000

    Events handling

--- README.md
+++ README.md
@@ -9,3 +9,39 @@ To start this project, open the `index.html` file on a browser.
 ## Presentation
 
 Introductory presentation to frontend development: https://docs.google.com/presentation/d/1wflP4ewVCN_I-h_Ur-8TVzBAungdy2RFb2P6enWkSDM/
+
+## Development
+
+`npm install` to install this project dependencies.
+
+`npm start` to start the development server.
+
+## Resources
+
+*Data*:
+
+- Todos list to fetch: http://www.mocky.io/v2/5e7a000b3000007800930554
+  - Returns the same content as `https://jsonplaceholder.typicode.com/todos` but with CORS headers.
+
+# Conclusions
+
+Disadvantages of this solution:
+
+- Code all on the same file;
+- No separation of concerns (view, model, events, data fetching);
+- Code gets messy very quickly;
+- Global vars (list, rootElement and functions);
+- State on the HTML (DOM);
+
+# Homework Challenges
+
+1. API returns some todos with `{ completed: true }`
+  - All todos with `completed=true` must have the class "done"
+
+2. Todos counter - 3/5 
+  - 3 is the number of uncompleted todos
+  - 5 is the number of total todos
+
+3. Support multiple instances of todos on the same page:
+  - Create a more modularized application;
+  - Reduce the number of global symbols that we currently have;
--- static/scripts.js
+++ static/scripts.js
@@ -1,3 +1,81 @@
-// My scripts
-
 console.log('Welcome to UMinho 2020 FE class')
+
+// find the elements
+const rootElement = document.getElementById('root');
+const list = rootElement.querySelector('.todo-list');
+
+// Add Todo Function
+function addTodo(description) {
+  // create todo `li` with `todo-item` class
+  const todoElem = document.createElement('li');
+  todoElem.setAttribute('class', 'todo-item');
+
+  // create description `span` with description text
+  const descriptionElem = document.createElement('span');
+  descriptionElem.setAttribute('class', 'description');
+  descriptionElem.innerText = description;
+
+  // register toggle click event listener
+  descriptionElem.addEventListener('click', event => toggleTodo(todoElem));
+
+  // create remove `span`
+  const removeElem = document.createElement('span');
+  removeElem.setAttribute('class', 'remove');
+  removeElem.innerText = '\u2716';
+
+  // register remove click event listener
+  removeElem.addEventListener('click', event => removeTodo(todoElem));
+
+  // add DOM elements to list
+  todoElem.appendChild(descriptionElem);
+  todoElem.appendChild(removeElem);
+  list.appendChild(todoElem);
+}
+
+addTodo('Learn CSS');
+addTodo('Learn HTML');
+addTodo('Learn JavaScript');
+
+function handleKeyPress(event) {
+  const value = event.target.value;
+  const key = event.key;
+
+  if (key === 'Enter' && value !== '') {
+    addTodo(value);
+    event.target.value = '';
+  }
+}
+
+// register keypress on the input
+const input = rootElement.querySelector('.add-todo');
+input.addEventListener('keypress', handleKeyPress);
+
+
+function toggleTodo(todoElem) {
+  todoElem.classList.toggle('done');
+}
+
+function removeTodo(todoElem) {
+  list.removeChild(todoElem);
+}
+
+function resetTodosList() {
+  //return fetch('https://jsonplaceholder.typicode.com/todos')
+  fetch('https://www.mocky.io/v2/5e7a000b3000007800930554')
+    .then(r => r.json())
+    .then(todosList => todosList.filter(todo => todo.userId === 1))
+    .then(todosList => {
+      console.log(todosList);
+      return todosList;
+    })
+    .then(todosList => {
+      list.innerHTML = "";
+      todosList.forEach(todo => {
+        addTodo(todo.title);
+      })
+    })
+}
+
+// register todos click handler
+const resetTodo = rootElement.querySelector('.reset-todos');
+resetTodo.addEventListener('click', resetTodosList);
